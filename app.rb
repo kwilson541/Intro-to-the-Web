569d5d906e27ebce9657abf4220aea242b66ff10
Kim Wilson <k.wilson541@gmail.com>
2016-11-22 17:20:09 +0000

    Added new get route

--- app.rb
+++ app.rb
@@ -23,8 +23,13 @@ get '/random-cat' do
 	erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
 	p params
 	@name = params[:name]
 	erb(:index)
+end
+
+get '/form' do
+	p params
+	erb(:form)
 end
