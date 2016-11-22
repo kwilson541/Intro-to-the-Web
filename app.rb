f7ea18514d88690a2905e857d08484b9c45dd650
Kim Wilson <k.wilson541@gmail.com>
2016-11-22 11:37:21 +0000

    Added Gemfile and basic app file

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+	'hello!'
+end
+
+get '/secret' do
+	"I like lemon curd and cheese crisp sandwiches."
+end
+
+get '/funny' do
+	"Why did the programmer quit his job? Because he didn't get arrays!"
+end
+
+get '/sleepy' do
+	"Is it time for bed yet?"
+end
